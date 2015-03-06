1732ae9866aac73a955f7d08db0c2c4ecfbe65e0
Darrell Jonathan Penta <d.penta@neu.edu>
2015-03-05 21:18:44 -0500

    Updated regressions; working on adding new column

--- Prosody regression.R
+++ Prosody regression.R
@@ -5,7 +5,10 @@ library(languageR)
 library(lme4)
 library(lmerTest)
 
-
+# ------------------------------SET UP INPUT FILE(S)----------------------------
+d.base <- read.csv("data/d_prosody.csv")
+as.factor(d.base$subject)
+as.factor(d.base$item)
 #------------------------------SET UP OUTPUT FILE------------------------------
 sink("output/SemRel Prosody Analyses.txt")
 
@@ -14,10 +17,7 @@ cat(" ", "\n")
 cat("SEMREL PROSODY ANALYSES RUN ON: ", format(Sys.time(), "%b. %d, %Y at %T"), sep = "", fill= 70)
 cat(" ", "\n")
 
-# ------------------------------SET UP INPUT FILE(S)----------------------------
-d.base <- read.csv("data/d_prosody.csv")
-as.factor(d.base$subject)
-as.factor(d.base$item)
+
 
 
 # -------------------CREATE SUBSETS--------------------------------------------
@@ -65,7 +65,7 @@ d.n2$assoc    <- d.n2$association
 # -------------------------------------------------------------------------------
 
 
-# D1-------------------
+# D1 -------------------
 cat(rep(c("-"), times=40, quote=F),"\n")
 cat("SUBSET D1", sep = "", fill = 60)
 cat(rep(c("-"), times=40, quote=F), "\n")
@@ -73,14 +73,143 @@ cat(rep(c("-"), times=40, quote=F), "\n")
 pros.d1 <- lmer(seconds ~  relat + integ  + assoc + plaus + (1|subject) + (1|item), data = d.d1, REML=FALSE)
 print(summary(pros.d1))
 
-# N1-------------------
+# N1 -------------------
 
 cat(rep(c("-"), times=40, quote=F),"\n")
 cat("SUBSET N1", sep = "", fill = 60)
 cat(rep(c("-"), times=40, quote=F), "\n")
 pros.n1 <- lmer(seconds ~  relat + integ  + assoc + plaus + (1|subject) + (1|item), data = d.n1, REML=FALSE)
 print(summary(pros.n1))
+
+# P1 -------------------
+
+cat(rep(c("-"), times=40, quote=F),"\n")
+cat("SUBSET P1", sep = "", fill = 60)
+cat(rep(c("-"), times=40, quote=F), "\n")
+pros.p1 <- lmer(seconds ~  relat + integ  + assoc + plaus + (1|subject) + (1|item), data = d.p1, REML=FALSE)
+print(summary(pros.p1))
+
+# D2 -------------------
+
+cat(rep(c("-"), times=40, quote=F),"\n")
+cat("SUBSET D2", sep = "", fill = 60)
+cat(rep(c("-"), times=40, quote=F), "\n")
+pros.d2 <- lmer(seconds ~  relat + integ  + assoc + plaus + (1|subject) + (1|item), data = d.d2, REML=FALSE)
+print(summary(pros.d2))
+
+# A1 -------------------
+
+cat(rep(c("-"), times=40, quote=F),"\n")
+cat("SUBSET A1", sep = "", fill = 60)
+cat(rep(c("-"), times=40, quote=F), "\n")
+pros.a1 <- lmer(seconds ~  relat + integ  + assoc + plaus + (1|subject) + (1|item), data = d.a1, REML=FALSE)
+print(summary(pros.a1))
+
+# N2 -------------------
+
+cat(rep(c("-"), times=40, quote=F),"\n")
+cat("SUBSET N2", sep = "", fill = 60)
+cat(rep(c("-"), times=40, quote=F), "\n")
+pros.n2 <- lmer(seconds ~  relat + integ  + assoc + plaus + (1|subject) + (1|item), data = d.n2, REML=FALSE)
+print(summary(pros.n2))
+
 sink()
 
 
 
+View(d.base)
+
+library(dplyr)
+library(reshape2)
+
+
+
+d <- read.csv("output/d_prosody.csv")
+View(d)
+d.val <- read.csv("data/prosody_items_original.csv")
+View(d.val)
+subtlex <- read.csv("data/subtlex.csv")
+View(subtlex)
+d.backup<-d
+
+
+# ----------------------------------------------------
+d.val$LengthPhon.Det <-2
+d.len.phon <- d.val[,c("subitem","LengthPhon.Det","LengthPhon.Head","LengthPhon.Prep","LengthPhon.Adj","LengthPhon.Det","LengthPhon.Noun")] #SUBSET OF MAIN SHEET
+d.len.phon<-melt(d.len.phon, id.vars=1) #create melted version of data
+View(d.len.phon)
+d.len.phon$test<-NA
+d.len.phon$test<-rep(c("D1","N1","P1","A1","D2","N2"), each = 698)
+d.len.phon$unique.id <- paste(d.len.phon[,1],d.len.phon[,4],sep="-") #concatenate string
+d <-left_join(d,d.len.phon, by="unique.id") #join b to ad
+# ----------------------------------------------------
+d.val$Length.Syl.Det <-1
+d.len.syll <- d.val[,c(1,68,24:25,68,26:27)] #SUBSET OF MAIN SHEET
+View(d.len.syll)
+d.len.syll<-melt(d.len.syll, id.vars=1) #create melted version of data
+View(d.len.syll)
+d.len.syll$test<-NA
+d.len.syll$test<-rep(c("D1","N1","P1","A1","D2","N2"), each = 698)
+d.len.syll$unique.id <- paste(d.len.syll[,1],d.len.syll[,4],sep="-") #concatenate string
+names(d)[names(d)=="value"] <- "len.syll"
+d.len.syll$test = NULL
+d.len.syll$variable = NULL
+d <-left_join(d,d.len.syll, by="unique.id") #join b to ad
+
+library(dplyr)
+library(reshape2)
+d <- read.csv("output/d_prosody.csv")
+
+d.val <- read.csv("data/prosody_items_original.csv")
+d.int <- d.val[,c(1,6)]
+d.int<-melt(d.int, id.vars=1)
+merge(d.base,d.int, by="subitem")
+View(d.base)
+d.phon <- d.val[,c(1,20:24)]
+d.phon$LengthSylHead = NULL
+d.phonm <-melt(d.phon, id.vars=1)
+
+d$word <- sapply(d$word, as.character)
+d$word[is.na(d$word)] <- " "
+as.data.frame(d$word)
+d$word<-tolower(d$word)
+
+# ---populates column with freq. values----
+d$freq<- "NA"
+d$freq <- sapply(d$freq, as.character)
+d$freq[is.na(d$freq)] <- " "
+as.data.frame(d$freq)
+
+freq <-
+  for (i in d$word) {
+    if(length(subtlex[subtlex$Word == i, 7]) == 0) {
+      d[d$word == i, 9] <- 0
+    }  else {
+      d[d$word == i, 9] <- subtlex[subtlex$Word ==i, 7]
+    }
+  }
+
+# ------
+
+d$len.char <- NA
+d$len.char <- nchar(d$word)
+
+# -----
+dys <-d.val[,c("subitem","dysfluency")]
+d<-merge(d,dys, by = "subitem")
+
+
+
+d$len.phon <- "NA"
+d$len.phon <- sapply(d$len.phon, as.character)
+d$len.phon[is.na(d$len.phon)] <- " "
+as.data.frame(d$len.phon)
+
+freq <-
+  for (i in d$word) {
+    if(length(subtlex[subtlex$Word == i, 7]) == 0) {
+      d[d$word == i, 9] <- 0
+    }  else {
+      d[d$word == i, 9] <- subtlex[subtlex$Word ==i, 7]
+    }
+  }
