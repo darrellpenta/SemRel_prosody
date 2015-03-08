9b6802cf815f070e3f7b3cf1de8a8814d0f2c9da
Darrell Jonathan Penta <d.penta@neu.edu>
2015-03-08 17:55:17 -0400

    Script, data file changed
    Based on meetings with Neal, the data file includes new columns for
    n1:n2 length, and w(+1/-1) freq.

--- Prosody regression.R
+++ Prosody regression.R
@@ -1,14 +1,32 @@
 # DP 3/5 Testing model
 
 rm(list=ls())
+#install.packages("corrplot")
+library(corrplot)
 library(languageR)
 library(lme4)
 library(lmerTest)
+
 
 # ------------------------------SET UP INPUT FILE(S)----------------------------
 d.base <- read.csv("data/d_prosody.csv")
 as.factor(d.base$subject)
 as.factor(d.base$item)
+d.base$freq      <- d.base$freq
+d.base$len.char  <- scale(d.base$len.char, center=TRUE, scale=TRUE)
+d.base$len.phon  <- scale(d.base$len.phon, center=TRUE, scale=TRUE)
+d.base$len.syll  <- scale(d.base$len.syll, center=TRUE, scale=TRUE)
+d.base$relat     <- scale(d.base$related, center=TRUE, scale=TRUE)
+d.base$integ     <- scale(d.base$integrated, center=TRUE, scale=TRUE)
+d.base$plaus     <- scale(d.base$plausibility, center=TRUE, scale=TRUE)
+d.base$assoc     <- d.base$association
+d.base$n1.len    <- as.numeric(d.base$n1.len)
+d.base$n1.len    <- scale(d.base$n1.len, center = TRUE, scale = TRUE)
+d.base$p1.len    <- scale(d.base$p1.len, center = TRUE, scale = TRUE)
+d.base$a1.len    <- scale(d.base$a1.len, center = TRUE, scale = TRUE)
+d.base$n2.len    <- scale(d.base$n2.len, center = TRUE, scale = TRUE)
+d.base$fr.pre.wd <- (as.numeric(d.base$pre.word.freq))
+d.base$fr.pos.wd <- (as.numeric(d.base$post.word.freq))
 
 # For meeting with Neal
 # View(d.base)
@@ -33,58 +51,6 @@ d.d2 <- subset(d.base, word.type == "D2")
 d.a1 <- subset(d.base, word.type == "A1")
 d.n2 <- subset(d.base, word.type == "N2")
 
-# --------------------PREPARE PREDICTOR VARIABLES FOR MODEL---------------------
-
-# D(et) 1 PREDICTORS
-d.d1$relat    <- scale(d.d1$related, center=TRUE, scale=TRUE)
-d.d1$integ    <- scale(d.d1$integrated, center=TRUE, scale=TRUE)
-d.d1$plaus    <- scale(d.d1$plausibility, center=TRUE, scale=TRUE)
-d.d1$assoc    <- d.d1$association
-
-# N(oun) 1 PREDICTORS
-d.n1$freq     <- d.n1$freq
-d.n1$len.char <- scale(d.n1$len.char, center=TRUE, scale=TRUE)
-d.n1$len.phon <- scale(d.n1$len.phon, center=TRUE, scale=TRUE)
-d.n1$len.syll <- scale(d.n1$len.syll, center=TRUE, scale=TRUE)
-d.n1$relat    <- scale(d.n1$related, center=TRUE, scale=TRUE)
-d.n1$integ    <- scale(d.n1$integrated, center=TRUE, scale=TRUE)
-d.n1$plaus    <- scale(d.n1$plausibility, center=TRUE, scale=TRUE)
-d.n1$assoc    <- d.n1$association
-
-# P(rep) 1 PREDICTORS
-d.p1$freq     <- d.p1$freq
-d.p1$len.char <- scale(d.p1$len.char, center=TRUE, scale=TRUE)
-d.p1$len.phon <- scale(d.p1$len.phon, center=TRUE, scale=TRUE)
-d.p1$relat    <- scale(d.p1$related, center=TRUE, scale=TRUE)
-d.p1$integ    <- scale(d.p1$integrated, center=TRUE, scale=TRUE)
-d.p1$plaus    <- scale(d.p1$plausibility, center=TRUE, scale=TRUE)
-d.p1$assoc    <- d.p1$association
-
-# D(et) 2 PREDICTORS
-d.d2$relat    <- scale(d.d2$related, center=TRUE, scale=TRUE)
-d.d2$integ    <- scale(d.d2$integrated, center=TRUE, scale=TRUE)
-d.d2$plaus    <- scale(d.d2$plausibility, center=TRUE, scale=TRUE)
-d.d2$assoc    <- d.d2$association
-
-# A(dj) 1 PREDICTORS
-d.a1$freq     <- d.a1$freq
-d.a1$len.char <- scale(d.a1$len.char, center=TRUE, scale=TRUE)
-d.a1$len.phon <- scale(d.a1$len.phon, center=TRUE, scale=TRUE)
-d.a1$len.syll <- scale(d.a1$len.syll, center=TRUE, scale=TRUE)
-d.a1$relat    <- scale(d.a1$related, center=TRUE, scale=TRUE)
-d.a1$integ    <- scale(d.a1$integrated, center=TRUE, scale=TRUE)
-d.a1$plaus    <- scale(d.a1$plausibility, center=TRUE, scale=TRUE)
-d.a1$assoc    <- d.a1$association
-
-# N(oun) 2 PREDICTORS
-d.n2$freq     <- d.n2$freq
-d.n2$len.char <- scale(d.n2$len.char, center=TRUE, scale=TRUE)
-d.n2$len.phon <- scale(d.n2$len.phon, center=TRUE, scale=TRUE)
-d.n2$len.syll <- scale(d.n2$len.syll, center=TRUE, scale=TRUE)
-d.n2$relat    <- scale(d.n2$related, center=TRUE, scale=TRUE)
-d.n2$integ    <- scale(d.n2$integrated, center=TRUE, scale=TRUE)
-d.n2$plaus    <- scale(d.n2$plausibility, center=TRUE, scale=TRUE)
-d.n2$assoc    <- d.n2$association
 
 
 # ------------------------------------------------------------------------------------------------------------------
@@ -94,56 +60,99 @@ d.n2$assoc    <- d.n2$association
 
 # D1 -------------------
 cat(rep(c("-"), times=40, quote=F),"\n")
-cat("SUBSET D1", sep = "", fill = 60)
+cat("SUBSET D1 (with Related)", sep = "", fill = 60)
 cat(rep(c("-"), times=40, quote=F), "\n")
-pros.d1 <- lmer(seconds ~  relat + integ  + assoc + plaus + (1|subject) + (1|item), data = d.d1, REML=FALSE)
-print(summary(pros.d1))
+pros.d1.r <- lmer(seconds ~  relat + integ  + plaus + n1.len + p1.len + a1.len + n2.len + fr.pos.wd + (1|subject) + (1|item), data = d.d1, REML=TRUE)
+print(summary(pros.d1.r))
+
+cat(rep(c("-"), times=40, quote=F),"\n")
+cat("SUBSET D1 (with Association)", sep = "", fill = 60)
+cat(rep(c("-"), times=40, quote=F), "\n")
+pros.d1.a <- lmer(seconds ~  integ  + assoc + plaus + n1.len + p1.len + a1.len + n2.len + fr.pos.wd + (1|subject) + (1|item), data = d.d1, REML=TRUE)
+print(summary(pros.d1.a))
+
 
 # N1 -------------------
 cat(rep(c("-"), times=40, quote=F),"\n")
-cat("SUBSET N1", sep = "", fill = 60)
+cat("SUBSET N1 (with Related)", sep = "", fill = 60)
 cat(rep(c("-"), times=40, quote=F), "\n")
-pros.n1 <- lmer(seconds ~  freq + len.char + len.phon + len.syll + relat + integ  + assoc + plaus + (1|subject) + (1|item), data = d.n1, REML=FALSE)
-print(summary(pros.n1))
+pros.n1.r <- lmer(seconds ~  freq + len.char + len.phon + len.syll + relat + integ + plaus + n1.len + p1.len + a1.len + n2.len + fr.pre.wd + fr.pos.wd + (1|subject) + (1|item), data = d.n1, REML=TRUE)
+print(summary(pros.n1.r))
+
+cat(rep(c("-"), times=40, quote=F),"\n")
+cat("SUBSET N1 (with Association)", sep = "", fill = 60)
+cat(rep(c("-"), times=40, quote=F), "\n")
+pros.n1.a <- lmer(seconds ~  freq + len.char + len.phon + len.syll + assoc + integ + plaus + n1.len + p1.len + a1.len + n2.len + fr.pre.wd + fr.pos.wd + (1|subject) + (1|item), data = d.n1, REML=TRUE)
+print(summary(pros.n1.a))
 
 
 # P1 -------------------
 cat(rep(c("-"), times=40, quote=F),"\n")
-cat("SUBSET P1", sep = "", fill = 60)
+cat("SUBSET P1 (with Related)", sep = "", fill = 60)
 cat(rep(c("-"), times=40, quote=F), "\n")
-pros.p1 <- lmer(seconds ~ freq + len.char + len.phon +  relat + integ  + assoc + plaus + (1|subject) + (1|item), data = d.p1, REML=FALSE)
-print(summary(pros.p1))
+pros.p1.r <- lmer(seconds ~ freq + len.char + len.phon +  relat + integ  + plaus + n1.len + p1.len + a1.len + n2.len + fr.pre.wd + fr.pos.wd +(1|subject) + (1|item), data = d.p1, REML=TRUE)
+print(summary(pros.p1.r))
+
+cat(rep(c("-"), times=40, quote=F),"\n")
+cat("SUBSET P1 (with Association)", sep = "", fill = 60)
+cat(rep(c("-"), times=40, quote=F), "\n")
+pros.p1.a <- lmer(seconds ~ freq + len.char + len.phon + integ  + assoc + plaus + n1.len + p1.len + a1.len + n2.len + fr.pre.wd + fr.pos.wd + (1|subject) + (1|item), data = d.p1, REML=TRUE)
+print(summary(pros.p1.a))
 
 # D2 -------------------
 cat(rep(c("-"), times=40, quote=F),"\n")
-cat("SUBSET D2", sep = "", fill = 60)
+cat("SUBSET D2 (with Related)", sep = "", fill = 60)
 cat(rep(c("-"), times=40, quote=F), "\n")
-pros.d2 <- lmer(seconds ~  relat + integ  + assoc + plaus + (1|subject) + (1|item), data = d.d2, REML=FALSE)
-print(summary(pros.d2))
+pros.d2.r <- lmer(seconds ~  relat + integ + plaus + n1.len + p1.len + a1.len + n2.len + fr.pre.wd + fr.pos.wd + (1|subject) + (1|item), data = d.d2, REML=TRUE)
+print(summary(pros.d2.r))
+
+cat(rep(c("-"), times=40, quote=F),"\n")
+cat("SUBSET D2 (with Association)", sep = "", fill = 60)
+cat(rep(c("-"), times=40, quote=F), "\n")
+pros.d2.a <- lmer(seconds ~ assoc + integ + plaus + n1.len + p1.len + a1.len + n2.len + fr.pre.wd + fr.pos.wd + (1|subject) + (1|item), data = d.d2, REML=TRUE)
+print(summary(pros.d2.a))
 
 # A1 -------------------
 
 cat(rep(c("-"), times=40, quote=F),"\n")
-cat("SUBSET A1", sep = "", fill = 60)
+cat("SUBSET A1 (with Related)", sep = "", fill = 60)
 cat(rep(c("-"), times=40, quote=F), "\n")
-pros.a1 <- lmer(seconds ~ freq + len.char + len.phon + len.syll + relat + integ  + assoc + plaus + (1|subject) + (1|item), data = d.a1, REML=FALSE)
-print(summary(pros.a1))
+pros.a1.r <- lmer(seconds ~ freq + len.char + len.phon + len.syll + relat + integ  + plaus + n1.len + p1.len + a1.len + n2.len + fr.pre.wd + fr.pos.wd + (1|subject) + (1|item), data = d.a1, REML=TRUE)
+print(summary(pros.a1.r))
+
+cat(rep(c("-"), times=40, quote=F),"\n")
+cat("SUBSET A1 (with Association)", sep = "", fill = 60)
+cat(rep(c("-"), times=40, quote=F), "\n")
+pros.a1.a <- lmer(seconds ~ freq + len.char + len.phon + len.syll  + integ  + assoc + plaus + n1.len + p1.len + a1.len + n2.len + fr.pre.wd + fr.pos.wd + (1|subject) + (1|item), data = d.a1, REML=TRUE)
+print(summary(pros.a1.a))
 
 # N2 -------------------
 cat(rep(c("-"), times=40, quote=F),"\n")
-cat("SUBSET N2", sep = "", fill = 60)
+cat("SUBSET N2 (with Related)", sep = "", fill = 60)
 cat(rep(c("-"), times=40, quote=F), "\n")
-pros.n2 <- lmer(seconds ~  freq + len.char + len.phon + len.syll + relat + integ  + assoc + plaus + (1|subject) + (1|item), data = d.n2, REML=FALSE)
-print(summary(pros.n2))
+pros.n2.r <- lmer(seconds ~  freq + len.char + len.phon + len.syll + relat + integ + plaus + n1.len + p1.len + a1.len + n2.len + fr.pre.wd + fr.pos.wd + (1|subject) + (1|item), data = d.n2, REML=TRUE)
+print(summary(pros.n2.r))
 
-# N2 -------------------
+cat(rep(c("-"), times=40, quote=F),"\n")
+cat("SUBSET N2 (with Association)", sep = "", fill = 60)
+cat(rep(c("-"), times=40, quote=F), "\n")
+pros.n2.a <- lmer(seconds ~  freq + len.char + len.phon + len.syll  + integ  + assoc + plaus + n1.len + a1.len + n2.len  + fr.pre.wd + fr.pos.wd +(1|subject) + (1|item), data = d.n2, REML=TRUE)
+print(summary(pros.n2.a))
+
+# CORRELATION MATRIX -------------------
 cat(rep(c("-"), times=40, quote=F),"\n")
 cat("CORRELATION MATRIX", sep = "", fill = 60)
 cat(rep(c("-"), times=40, quote=F), "\n")
-d.cor<-d.base[,c(5,9:16)]
+d.cor<-d.base[,c(5, 9:16, 19:22,29:30)]
 d.cor<-subset(d.cor, word.type =="D1" | word.type == "N1" | word.type == "P1" | word.type == "D2" | word.type == "A1"| word.type=="N2")
 d.cor$word.type = NULL
-cor.mat <-cor(d.cor)
+cor.mat <-cor(d.cor, use="complete")
 print(cor.mat, digits=4)
+
+jpeg(file="figures/correlation_matrix.jpg")
+plot.title = paste("Semrel Prosody Correlation Matrix")
+corrplot(cor.mat, method = "circle", main = plot.title, mar=c(1,1,2,1))
+dev.off()
+
 sink()
 
